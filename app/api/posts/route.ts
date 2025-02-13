fc66d4ddc2af490ef7a6295e8721c6a5e1665e64
Manula Pasan <89297752+Manula413@users.noreply.github.com>
2025-02-13 13:28:24 +0530

    Removed unnecessary  console logs

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,13 +3,13 @@ import { connectToDatabase } from "../../../lib/mongodb";
 export async function GET() {
   try {
     const db = await connectToDatabase();
-    const collection = db.collection("posts"); // Get the posts collection
-    console.log("Connected to collection:", collection);  // Log collection object
+    const collection = db.collection("posts"); 
+    
     const posts = await collection.find({}).toArray();
-    console.log("Fetched posts:", posts);
+    
     return new Response(JSON.stringify(posts), { status: 200 });
   } catch (error) {
-    console.error("Error fetching posts:", error);
+    
     return new Response(JSON.stringify({ message: "Failed to fetch posts" }), { status: 500 });
   }
 }
